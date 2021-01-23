e8af2c2aa6d9008ef11b027286e323ea88f3b029
Noé Iván Vivero Guzmán <NoeAVGN@outlook.com>
2021-01-23 10:54:27 -0600

    Initial commit empty project

---
+++ MeetingRoomManager/src/main/java/com/noe/manager/meetingroom/MeetingRoomManagerApplication.java
@@ -0,0 +1,13 @@
+package com.noe.manager.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomManagerApplication.class, args);
+	}
+
+}
---
+++ MeetingRoomManager/src/test/java/com/noe/manager/meetingroom/MeetingRoomManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noe.manager.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
